e978598c2f10764a6ff85f6cd6ceb4ce1518b217
andytao05 <andytaodev@gmail.com>
2018-06-09 19:42:54 +0800

    Update with comments

--- app/src/main/java/sg/edu/rp/c346/simplesave/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/simplesave/MainActivity.java
@@ -12,6 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // This is a new line
+
     }
 
     @Override
